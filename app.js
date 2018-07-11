ac17523292bb0d41198dbf0fadcf258b6370edc1
pyszny <maciej.sawicky@gmail.com>
2018-07-11 16:44:53 +0200

    Added comments to app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require('http-errors');                                   //https://www.npmjs.com/package/http-errors
+var express = require('express');                                           //https://www.npmjs.com/package/express
+var path = require('path');                                                 //https://www.npmjs.com/package/path
+var cookieParser = require('cookie-parser');                                //https://www.npmjs.com/package/cookie-parser
+var logger = require('morgan');                                             //https://www.npmjs.com/package/morgan
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,14 +13,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));                                                     //Concise output colored by response status for development use
+app.use(express.json());                                                    //https://expressjs.com/en/api.html#express.json
+app.use(express.urlencoded({ extended: false }));                           //parsing the URL-encoded data with the querystring library
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));                    //determines the file to serve static files such as images, CSS files, and JavaScript files by combining req.url with the provided root directory
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter);                                                  //
+app.use('/users', usersRouter);                                             //loads router modules
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
